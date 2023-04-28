7bedf0a7e0165b89632d38fc8fa4fcb51572859f
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-04-29 01:28:15 +0300

    Create copy-paster.py

---
+++ copy-paster.py
@@ -0,0 +1,3 @@
+inp = input("\nGive me a text you want to copy:")
+inp_times = int(input("How many times do you want to copy it?"))
+print(f"{str(inp)} " * inp_times, "\n")  # restricted functionality
