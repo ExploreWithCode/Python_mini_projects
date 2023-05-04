f4e4df2aa15bfea8e52c0603377c98a6ba2df9b8
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-05-05 00:40:26 +0300

    Update integer_finder.py

--- integer_finder.py
+++ integer_finder.py
@@ -1,19 +1,19 @@
 accepted_replies = ["yes", "y", "YES", "Y", "Yes"]
 print("This tool helps you find all the integers between (and excluding) two given numbers.")
-i = int(input("Insert the initial number:"))
-m = int(input("Insert the maximum number:"))
+min = int(input("Insert the minimum number:"))
+max = int(input("Insert the maximum number:"))
 choice = input("Do you want these numbers to be included in the result?")
-if i >= m:
-    print("The initial number has to be smaller than the maximum!")
+if min >= max:
+    print("The minimum number has to be smaller than the maximum!")
 print("The numbers you requested are these:")
 if choice in accepted_replies:
-    while i <= m:
-        print(i, end=" ")
-        i += 1
+    while min <= max:
+        print(min, end=" ")
+        min += 1
 else:
-    if m - i <= 1:
-        print("If the distance between the initial and the maximum is not bigger than 1, no number gets printed.")
-    while i < m - 1:
-        i += 1
-        print(i, end=" ")
-       
+    if max - min <= 1:
+        print("If the distance between the minimum and the maximum is not bigger than 1, no number gets printed.")
+    while min < max - 1:
+        min += 1
+        print(min, end=" ")
+
