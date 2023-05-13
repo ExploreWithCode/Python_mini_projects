d27b8408c4e3c370a1a2dbcab9e25f6791ba3e2d
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-05-13 19:07:01 +0300

    Update Python_encryption_idea.py

--- Python_encryption_idea.py
+++ Python_encryption_idea.py
@@ -2,16 +2,18 @@ en_key = 1
 enk = input("Type something and I will use it to encrypt the next text: \n")
 en_key2 = len(enk)
 
+
 def encrypt(text):
     result = ""
     for char in text:
         if char.isalpha():
             alphabet = 'abcdefghijklmnopqrstuvwxyz' if char.islower() else 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-            shifted = alphabet[(alphabet.index(char) + en_key + en_key2) % 26]
+            shifted = alphabet[(alphabet.index(char) + pow(en_key, en_key2) + (en_key + en_key2)*2) % 26]
             result += shifted
         else:
             result += char
     return result
 
 
-print(encrypt(input("Enter the text you want to encrypt: \n")))
+res = encrypt(input("Enter the text you want to encrypt: \n"))  # 1st encryption
+print(encrypt(res))  # 2nd encryption
