3fc9065cf4c28e108a9cfbd1569ff78862d79d92
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-05-16 21:03:28 +0300

    Create life expectancy.py

---
+++ life expectancy.py
@@ -0,0 +1,16 @@
+both = float(73.2)
+females = float(75.6)
+males = float(70.8)
+print("The average life expectancy of a human in general right now is", both, "years.")
+print("Enter your age: ")
+age = input()
+per_of_both = (int(age)/int(both)*100)
+per_of_females = (int(age)/int(females)*100)
+per_of_males = (int(age)/int(males)*100)
+if int(age) < 0:
+    print("Pick a valid number")
+else:
+    print("You have lived this percentage of your expected life according to worldwide data for:")
+    print("both sexes :", round(per_of_both, 2), "%")
+    print("females :", round(per_of_females, 2), "%")
+    print("males :", round(per_of_males, 2), "%")
