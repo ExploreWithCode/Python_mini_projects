1111854d339271bc44f05bc808b99482309ae530
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-05-18 01:17:35 +0300

    Update and rename Python_encryption_idea.py to encryption-decryption/encryption-decryption.py

--- encryption-decryption/encryption-decryption.py
+++ encryption-decryption/encryption-decryption.py
@@ -16,4 +16,19 @@ def encrypt(text):
 
 
 res = encrypt(input("Enter the text you want to encrypt: \n"))  # 1st encryption
-print(encrypt(res))  # 2nd encryption
+print(f"Encrypted message: {res}")
+
+
+def decrypt(text):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            alphabet = 'abcdefghijklmnopqrstuvwxyz' if char.islower() else 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            shifted = alphabet[(alphabet.index(char) - pow(en_key, en_key2) - (en_key + en_key2)*2) % 26]
+            result += shifted
+        else:
+            result += char
+    return result
+
+
+print(f"Decrypted message: {decrypt(res)}")
