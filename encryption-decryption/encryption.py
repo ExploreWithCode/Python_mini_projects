b42e064fcc58c6716143e6608128c352b9341e9b
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-05-19 20:54:03 +0300

    Create encryption.py

---
+++ encryption-decryption/encryption.py
@@ -0,0 +1,17 @@
+import keys
+
+
+def encrypt(text):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            alphabet = 'abcdefghijklmnopqrstuvwxyz' if char.islower() else 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            shifted = alphabet[(alphabet.index(char) + pow(keys.en_key, keys.en_key2) + (keys.en_key + keys.en_key2)*2) % 26]
+            result += shifted
+        else:
+            result += char
+    return result
+
+
+res = encrypt(input("Enter the text you want to encrypt: \n"))  # encryption
+print(f"Encrypted message: {res}")
