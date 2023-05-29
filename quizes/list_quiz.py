83ee42df045018e0dc9d945b22b5a9037a43548d
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-05-29 16:31:27 +0300

    Update list_quiz.py

--- quizes/list_quiz.py
+++ quizes/list_quiz.py
@@ -2,16 +2,20 @@ points = 0
 print("Find an item in a given list. Use uppercase and lowercase appropriately.")
 scand_cntrs = ["Norway", "Finland", "Sweden"]
 inp1 = input("1) Scandinavian countries: ")
-if inp1 in scand_cntrs:
+if inp1.isalpha() == False:
+    print("Not an accepted answer!")
+elif inp1 in scand_cntrs:
     points += 1
     print("Got it!")
 else:
     print("Not in the list")
-scand_cntrs = ["Vatican City", "Monaco", "San Marino", "Liechtenstein", "Malta", "Andorra"]
+eur_micros = ["Vatican City", "Monaco", "San Marino", "Liechtenstein", "Malta", "Andorra"]
 inp2 = input("2) European microstates: ")
-if inp2 in scand_cntrs:
+if inp2 in eur_micros:
     points += 1
     print("Got it!")
+elif inp2.isalpha() == False:
+    print("Not an accepted answer!")
 else:
     print("Not in the list")
 questions = (inp1, inp2)
