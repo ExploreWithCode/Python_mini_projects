d1271ff85f332a659b6c6ef39af731ba8fb9d84c
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-06-03 18:36:01 +0300

    Update keys2.py

--- encryption-decryption/keys2.py
+++ encryption-decryption/keys2.py
@@ -1,8 +1,10 @@
 import random
 
 en_key = 1
-mylist = ["a", "an", "ant", "word", "count", "letter", "numbers", "counting", "stabilize", "encryption", "randomizing", "destabilized", "technological",\
-          "randomizations", "diversification", "decentralization", "collateralization", "overcollateralized", "undercollateralized", "underdiversification",\
-          "overcollateralization", "undercollateralization", "electroencephalographer", "electroencephalographist", "cholinephosphotransferase", "N-arachidonoylethanolamine"]
+mylist = ["a", "an", "ant", "word", "count", "letter", "numbers", "counting", "stabilize", "encryption", "randomizing",
+          "destabilized", "technological", "randomizations", "diversification", "decentralization", "collateralization",
+          "overcollateralized", "undercollateralized", "underdiversification", "overcollateralization",
+          "undercollateralization", "electroencephalographer", "electroencephalographist", "cholinephosphotransferase",
+          "N-arachidonoylethanolamine"]
 enk = random.choice(mylist)  # a random word with length from 1 to 24
 en_key2 = len(enk)
