8c6f6e041b7fcaa83210c7fc4eaabf77681dade4
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-06-07 20:30:55 +0300

    Update Alphabet_indexing.py

--- Alphabet_indexing.py
+++ Alphabet_indexing.py
@@ -1,60 +1,58 @@
-A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z  = \
-1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26
-a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z = \
-1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26
-print("Enter a letter (CAPITAL or small) and receive its order in the aplhabet:")
+a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = \
+ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26
+print("Enter a letter (CAPITAL or small) and receive its order in the alphabet:")
 inp = input()
-if inp == "a" or inp == "A": #1
+if inp == "a" or inp == "A":  # 1
     print(a)
-elif inp == "b" or inp == "B": #2
+elif inp == "b" or inp == "B":  # 2
     print(b)
-elif inp == "c" or inp == "C": #3
+elif inp == "c" or inp == "C":  # 3
     print(c)
-elif inp == "d" or inp == "D": #4
+elif inp == "d" or inp == "D":  # 4
     print(d)
-elif inp == "e" or inp == "E": #5
+elif inp == "e" or inp == "E":  # 5
     print(e)
-elif inp == "f" or inp == "F": #6
+elif inp == "f" or inp == "F":  # 6
     print(f)
-elif inp == "g" or inp == "G": #7
+elif inp == "g" or inp == "G":  # 7
     print(g)
-elif inp == "h" or inp == "H": #8
+elif inp == "h" or inp == "H":  # 8
     print(h)
-elif inp == "i" or inp == "I": #9
+elif inp == "i" or inp == "I":  # 9
     print(i)
-elif inp == "j" or inp == "J": #10
+elif inp == "j" or inp == "J":  # 10
     print(j)
-elif inp == "k" or inp == "K": #11
+elif inp == "k" or inp == "K":  # 11
     print(k)
-elif inp == "l" or inp == "L": #12
+elif inp == "l" or inp == "L":  # 12
     print(l)
-elif inp == "m" or inp == "M": #13
+elif inp == "m" or inp == "M":  # 13
     print(m)
-elif inp == "n" or inp == "N": #14
+elif inp == "n" or inp == "N":  # 14
     print(n)
-elif inp == "o" or inp == "O": #15
+elif inp == "o" or inp == "O":  # 15
     print(o)
-elif inp == "p" or inp == "P": #16
+elif inp == "p" or inp == "P":  # 16
     print(p)
-elif inp == "q" or inp == "Q": #17
+elif inp == "q" or inp == "Q":  # 17
     print(q)
-elif inp == "r" or inp == "R": #18
+elif inp == "r" or inp == "R":  # 18
     print(r)
-elif inp == "s" or inp == "S": #19
+elif inp == "s" or inp == "S":  # 19
     print(s)
-elif inp == "t" or inp == "T": #20
+elif inp == "t" or inp == "T":  # 20
     print(t)
-elif inp == "u" or inp == "U": #21
+elif inp == "u" or inp == "U":  # 21
     print(u)
-elif inp == "v" or inp == "V": #22
+elif inp == "v" or inp == "V":  # 22
     print(v)
-elif inp == "w" or inp == "W": #23
+elif inp == "w" or inp == "W":  # 23
     print(w)
-elif inp == "x" or inp == "X": #24
+elif inp == "x" or inp == "X":  # 24
     print(x)
-elif inp == "y" or inp == "Y": #25
+elif inp == "y" or inp == "Y":  # 25
     print(y)
-elif inp == "z" or inp == "Z": #26
+elif inp == "z" or inp == "Z":  # 26
     print(z)
 else:
     print("Choose one letter from 'A' to 'Z' or from 'a' to 'z'")
