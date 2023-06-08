308d56e72b842d43c0b0e667eaa1143bd7fbd41d
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-06-08 21:10:26 +0300

    Update multiple_choice_quiz.py

--- quizes/multiple_choice_quiz.py
+++ quizes/multiple_choice_quiz.py
@@ -1,7 +1,7 @@
 points = 0
 accepted = "ABCDabcd"
 print("This is a multiple choice quiz. Answer correctly to get points.")
-print("Which of the following is NOT a fully supported Python programming paradigm?")
+print("1) Which of the following is NOT a fully supported Python programming paradigm?")
 inp1 = input("A: object-oriented | B: structured | C: logic | D: all of the above\n")
 if inp1 not in accepted:
     print("Not an accepted answer!")
@@ -10,7 +10,7 @@ elif inp1 == "C" or inp1 == "c":
     print("Correct!")
 else:
     print("Wrong answer!")
-print("Name the collection of principles that influence the design of Python:")
+print("2) Name the collection of principles that influence the design of Python:")
 inp2 = input("A: Python Guiding Principles | B: Zen of Python | C: Principles For The Snake | D: The Python's Principles\n")
 if inp2 not in accepted:
     print("Not an accepted answer!")   
@@ -19,7 +19,7 @@ elif inp2 == "B" or inp2 == "b":
     print("Correct!")
 else:
     print("Wrong answer!")
-print("Python was first released in:")
+print("3) Python was first released in:")
 inp3 = input("A: 1980 | B: 1991 | C: 2000 | D: 2008\n")
 if inp3 not in accepted:
     print("Not an accepted answer!")    
@@ -31,4 +31,4 @@ else:
 questions = (inp1, inp2, inp3)
 q = len(questions)
 print(f"Success rate: {round((points / q) * 100, 2)}%")
-print(f"Total points = {points}")
+print(f"Total points = {points}/{q}")
