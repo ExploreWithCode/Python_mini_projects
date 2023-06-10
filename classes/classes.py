64197849499db8ba3d071f1856e64a5c0df7ada8
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-06-10 11:21:26 +0300

    Create classes.py
    a file that contains classes to be used in another file

---
+++ classes/classes.py
@@ -0,0 +1,6 @@
+class Character:
+    def __init__(self, name, interest, age, is_alive):
+        self.name = name
+        self.interest = interest
+        self.age = age
+        self.is_alive = is_alive
