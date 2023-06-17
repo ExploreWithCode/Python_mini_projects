f78d92e8bc28490a3573107d25315005cc9f9ede
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-06-17 19:32:02 +0300

    Create encryption-decryption_2.py
    encryption and decryption of uppercase are different from those of lowercase

---
+++ encryption-decryption/encryption-decryption_2.py
@@ -0,0 +1,42 @@
+en_key = 1
+enk = input("Type something and I will use it to encrypt the next text: \n")
+en_key2 = len(enk)
+
+
+def encrypt(text):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                alphabet = 'abcdefghijklmnopqrstuvwxyz'
+                shifted = alphabet[(alphabet.index(char) + pow(en_key, en_key2) + (en_key + en_key2)*2) % 26]
+            else:
+                alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                shifted = alphabet[(alphabet.index(char) + pow(en_key, en_key2) + (en_key + en_key2 + 1) * 2) % 26]
+            result += shifted
+        else:
+            result += char
+    return result[::-1]  # reversal of string
+
+
+res = encrypt(input("Enter the text you want to encrypt: \n"))  # encryption
+print(f"Encrypted message: {res}")
+
+
+def decrypt(text):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                alphabet = 'abcdefghijklmnopqrstuvwxyz'
+                shifted = alphabet[(alphabet.index(char) - pow(en_key, en_key2) - (en_key + en_key2) * 2) % 26]
+            else:
+                alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                shifted = alphabet[(alphabet.index(char) - pow(en_key, en_key2) - (en_key + en_key2 + 1) * 2) % 26]
+            result += shifted
+        else:
+            result += char
+    return result[::-1]  # reversal of string to original direction
+
+
+print(f"Decrypted message: {decrypt(res)}")
