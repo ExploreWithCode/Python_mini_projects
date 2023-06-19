0dd7dd4a82a408d4c54133309ef77b2c3cba79d0
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-06-19 21:01:09 +0300

    Create decryption_2.py

---
+++ encryption-decryption/decryption_2.py
@@ -0,0 +1,21 @@
+import keys
+
+
+def decrypt(text):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                alphabet = 'abcdefghijklmnopqrstuvwxyz'
+                shifted = alphabet[(alphabet.index(char) - pow(keys.en_key, keys.en_key2) - (keys.en_key + keys.en_key2) * 2) % 26]
+            else:
+                alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                shifted = alphabet[(alphabet.index(char) - pow(keys.en_key, keys.en_key2) - (keys.en_key + keys.en_key2 + 1) * 2) % 26]
+            result += shifted
+        else:
+            result += char
+    return result[::-1]  # reversal of string to original direction
+
+
+res2 = decrypt(input("Enter the text you want to decrypt: \n"))  # decryption
+print(f"Decrypted message: {res2}")
