588fe0edb775979f2d9abfcbd3d7943995c5273d
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-07-01 12:46:58 +0300

    Create list_quiz_with_classes.py

---
+++ quizes/list_quiz_with_classes.py
@@ -0,0 +1,31 @@
+from Question import QuestionL
+question_prompts = [
+    "1) Scandinavian countries: \n",
+    "2) European microstates: \n"
+]
+q = len(question_prompts)
+scand_cntrs = ["Norway", "Finland", "Sweden"]
+eur_micros = ["Vatican City", "Monaco", "San Marino", "Liechtenstein", "Malta", "Andorra"]
+questions = [
+    QuestionL(question_prompts[0], scand_cntrs),
+    QuestionL(question_prompts[1], eur_micros),
+]
+print("Find an item in a given list. Use uppercase and lowercase appropriately.")
+
+
+def que(qstns):
+    points = 0
+    for question in qstns:
+        answer = input(question.prompt)
+        if answer.isnumeric():
+            print("Not an accepted answer!")
+        elif answer in question.lis:
+            points += 1
+            print("Correct!")
+        else:
+            print("Wrong answer!")
+    print(f"Success rate: {round((points / q) * 100, 2)}%")
+    print(f"Total points = {points} / {q}")
+
+
+que(questions)
