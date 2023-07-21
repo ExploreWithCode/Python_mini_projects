95f0735928ea5468cdb3456433b9aa80a3def202
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-07-21 21:05:51 +0300

    Create number printing.py

---
+++ number printing.py
@@ -0,0 +1,8 @@
+print("Which numbers do you want to print?")
+print("Give me the starting number: ")
+st = int(input())
+print("Give me the ending number: ")
+en = int(input())
+while st <= en:
+    print(st, end=" ")
+    st += 1
