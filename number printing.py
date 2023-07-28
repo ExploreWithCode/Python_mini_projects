3b9054c3f3f2ae47068c166124b60ed851c2ec5c
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-07-28 22:18:29 +0300

    Update number printing.py

--- number printing.py
+++ number printing.py
@@ -3,6 +3,8 @@ print("Give me the starting number: ")
 st = int(input())
 print("Give me the ending number: ")
 en = int(input())
+if st > en:
+    print("The starting number has to be smaller than the ending number.")
 while st <= en:
     print(st, end=" ")
     st += 1
