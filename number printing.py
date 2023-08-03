81cbbf8c8ace81aea3c609b67777c7842a0bb2c4
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-08-04 01:59:34 +0300

    Update number printing.py

--- number printing.py
+++ number printing.py
@@ -1,10 +1,20 @@
+vls = []
+brk = ["N", "n"]
+asdf = ""
 print("Which numbers do you want to print?")
-print("Give me the starting number: ")
-st = int(input())
-print("Give me the ending number: ")
-en = int(input())
-if st > en:
-    print("The starting number has to be smaller than the ending number.")
-while st <= en:
-    print(st, end=" ")
-    st += 1
+while asdf is not brk:
+    try:
+        print("Give me the starting integer number: ")
+        st = int(input())
+        print("Give me the ending integer number: ")
+        en = int(input())
+        if st > en:
+            print("The starting number has to be smaller than the ending number.")
+        while st <= en:
+            print(st, end=" ")
+            st += 1
+        conf = input("\nPress anything to continue or 'N' quit. ")
+        if conf in brk:
+            break
+    except ValueError:
+        print("Invalid input")
