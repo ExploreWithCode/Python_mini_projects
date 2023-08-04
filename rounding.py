59637dcbfdb81e7681302dca27b4335a6b2d39bf
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-08-04 21:25:35 +0300

    Update rounding.py

--- rounding.py
+++ rounding.py
@@ -1,5 +1,11 @@
 # Customized rounding of a number
-num = float(input("Give me a number to round: "))
-r_n = int(input("By how many decimals do you want to round up the number you gave me? "))
-rounded = round(num, r_n)
-print(rounded)
+brk = ["N", "n"]
+asdf = ""
+while asdf is not brk:
+    num = float(input("Give me a number to round: "))
+    r_n = int(input("By how many decimals do you want to round up the number you gave me? "))
+    rounded = round(num, r_n)
+    print(rounded)
+    conf = input("Press anything to continue or 'N' to stop adding numbers. ")
+    if conf in brk:
+        break
