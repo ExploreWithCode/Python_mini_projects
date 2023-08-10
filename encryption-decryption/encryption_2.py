59d44863c83d65dea2a6ee64ddbaa4e6ae1c3346
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-08-10 20:42:51 +0300

    added numbers

--- encryption-decryption/encryption_2.py
+++ encryption-decryption/encryption_2.py
@@ -12,6 +12,10 @@ def encrypt(text):
                 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
                 shifted = alphabet[(alphabet.index(char) + pow(keys.en_key, keys.en_key2) + (keys.en_key + keys.en_key2 + 1)*2) % 26]
             result += shifted
+        elif char.isnumeric():
+            numbers = '1234567890'
+            shifted = numbers[(numbers.index(char) + pow(keys.en_key, keys.en_key2) + (keys.en_key + keys.en_key2 + 2)*2) % 10]
+            result += shifted
         else:
             result += char
     return result[::-1]  # reversal of string
