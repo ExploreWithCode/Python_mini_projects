2f85973e6fa0d770fb5ccb560f129ab02f412ae4
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-08-16 17:52:44 +0300

    added histogram

--- Machine Learning/data distribution.py
+++ Machine Learning/data distribution.py
@@ -1,4 +1,5 @@
 import numpy
+import matplotlib.pyplot as plt
 print("Give me the minimum value: ")
 low = int(input())
 print("Give me the maximum value: ")
@@ -7,3 +8,5 @@ print("Give me the value size: ")
 size = int(input())
 x = numpy.random.uniform(low, high, size)
 print(x)
+plt.hist(x, size)
+plt.show()
