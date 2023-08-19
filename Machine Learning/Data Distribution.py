dc4e6acb1cad41cfbaae41519530966a8a7ed12c
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-08-19 20:06:54 +0300

    Update and rename data distribution.py to Data Distribution.py

--- Machine Learning/Data Distribution.py
+++ Machine Learning/Data Distribution.py
@@ -1,3 +1,4 @@
+from statistics import mode
 import numpy
 import matplotlib.pyplot as plt
 print("Give me the minimum value: ")
@@ -7,6 +8,14 @@ high = int(input())
 print("Give me the value size: ")
 size = int(input())
 x = numpy.random.uniform(low, high, size)
-print(x)
-plt.hist(x, size)
+
+md = mode(x)  # most common (in case of a tie, the first value is printed)
+print(f"Mode: {md}")
+
+if size < 10:
+    plt.hist(x, size)
+elif size < 100:
+    plt.hist(x, 10)
+else:
+    plt.hist(x, 100)
 plt.show()
