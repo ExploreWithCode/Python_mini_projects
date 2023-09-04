6770a69f4aede0eaa85d919202e0dfce7938be4e
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-09-04 20:04:19 +0300

    Update typing_accuracy_tester.py
    dictionary reform

--- everyday_life/typing_accuracy_tester.py
+++ everyday_life/typing_accuracy_tester.py
@@ -1,6 +1,13 @@
 import random
-
-dictionary = ["man", "library", "see", "interstellar", "odyssey", "journey", "speculation", "rectangular"]
+l2 = ["to", "at", "me", "an", "it", "is", "in", "if", "on", "or", "of", "am", "be", "no", "my", "hi", "oh", "he", "ex"]
+l3 = ["off", "man", "men", "for", "boy", "ant", "arm", "art", "bee", "say", "act", "not", "law", "old", "all", "key"]
+l4 = ["from", "four", "room", "boom", "zoom", "loot", "bard", "task", "coin", "sign", "mint", "that", "note", "want"]
+l5 = ["bloom", "words", "world", "scans", "basic", "merit", "force", "token", "banks", "after", "apply", "quote", "state"]
+l6 = ["cancer", "skills", "frozen", "strong", "muscle", "common", "videos", "bureau", "before", "report", "policy"]
+l7 = ["scandal", "promise", "falling", "fragile", "magical", "digital", "towards", "warning", "because", "custody"]
+l8 = ["illiquid", "provider", "progress", "vampires", "werewolf", "investor", "monetary", "national", "reserve", "project"]
+l9 = ["liquidity", "anonymity", "roadmaps", "investors", "framework", "sentiment", "president", "regulation", "statement"]
+dictionary = l2 + l3 + l4 + l5 + l6 + l7 + l8 + l9
 print("Type the word you see.")
 points = 0
 words_used = 0
