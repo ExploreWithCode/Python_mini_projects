103d515d6fe3ffcc4d76f691cd5414e7645179e7
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-09-12 22:20:11 +0300

    Update length_check.py

--- everyday_life/length_check.py
+++ everyday_life/length_check.py
@@ -3,4 +3,4 @@ print(len(inp))
 print("Give me a letter or word to know when it first appears in the text")
 ind = input("")
 # Python starts indexing with 0 instead of 1, so I add 1 to counteract this.
-print(f"\"{ind}\" first appears at character {inp.index(ind) + 1}")
+print(f"\"{ind}\" first appears at position {inp.index(ind) + 1}")
