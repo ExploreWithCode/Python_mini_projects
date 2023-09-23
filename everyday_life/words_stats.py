4867ad54d61bb36e17c6a7c4550bf4e7d8e36995
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-09-23 13:38:13 +0300

    Update words_stats.py

--- everyday_life/words_stats.py
+++ everyday_life/words_stats.py
@@ -2,14 +2,19 @@ from statistics import mode
 import numpy
 txt = input("Give me some text:\n")
 print(f"The text contains {len(txt.split())} words.")
-
 number_of_occurrences = 0
 length = []
+txt = txt.lower()
 for word in txt.split():
     l_w = len(word)
     if word == mode(txt.split()):
         number_of_occurrences += 1
     length.append(l_w)
-
-print(f"\"{mode(txt.split())}\" was the most common word and was mentioned {number_of_occurrences} times.")
-print(f"The average word length was {int(numpy.mean(length))} characters.")
+if number_of_occurrences == 1:
+    print(f"\"{mode(txt.split())}\" was the most common word and was mentioned {number_of_occurrences} time.")
+else:
+    print(f"\"{mode(txt.split())}\" was the most common word and was mentioned {number_of_occurrences} times.")
+if int(numpy.mean(length)) == 1:
+    print(f"The average word length was {int(numpy.mean(length))} character.")
+else:
+    print(f"The average word length was {int(numpy.mean(length))} characters.")
