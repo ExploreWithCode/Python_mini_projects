a9dff969bc3e816ab93e18dcdd4fe1561e042f2a
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-09-25 23:58:39 +0300

    Update word_points.py

--- for_fun/word_points.py
+++ for_fun/word_points.py
@@ -3,7 +3,6 @@ print("This is a tool that finds the word with the highest value within a given 
       "1) A word gets points based on the letters it contains. \"a\"=1, \"b\"=2 etc.\n"
       "2) The numbers are added and show the value of the word.\n"
       "3) In case of a tie, the word with the least characters gets printed.\n")
-
 txt = input("Give me some text:\n")
 txt = txt.lower()
 values = []
@@ -24,7 +23,6 @@ for word in txt.split():
             max_words.append(word)
       elif word_value == min(values):
             min_words.append(word)
-
 print(f"\"{max_words[-1]}\" was the most valuable word with a value of {max(values)}.")
 print(f"\"{min_words[-1]}\" was the least valuable word with a value of {min(values)}.")
 print(f"The average word value is {round(numpy.mean(values))}.")
