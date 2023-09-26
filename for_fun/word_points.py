1f8365432b2b1c4bfe6e65dc6441725212ddb924
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-09-26 03:54:39 +0300

    Update word_points.py
    optimized the if statements

--- for_fun/word_points.py
+++ for_fun/word_points.py
@@ -5,9 +5,7 @@ print("This is a tool that finds the word with the highest value within a given 
       "3) In case of a tie, the word with the least characters gets printed.\n")
 txt = input("Give me some text:\n")
 txt = txt.lower()
-values = []
-max_words = []
-min_words = []
+values, max_words, min_words = [], [], []
 for word in txt.split():
       word_value = 0
       for char in word:
@@ -16,13 +14,13 @@ for word in txt.split():
                   char_value = alphabet.index(char) + 1
                   word_value += char_value
       values.append(word_value)
-      if word_value == max(values) and word_value == min(values):
+      if word_value == max(values) and word_value == min(values) and word not in max_words and word not in min_words:
             max_words.append(word)
             min_words.append(word)
-      elif word_value == max(values):
+      elif word_value == max(values) and word not in max_words:
             max_words.append(word)
-      elif word_value == min(values):
+      elif word_value == min(values) and word not in min_words:
             min_words.append(word)
-print(f"\"{max_words[-1]}\" was the most valuable word with a value of {max(values)}.")
+print(f"\n\"{max_words[-1]}\" was the most valuable word with a value of {max(values)}.")
 print(f"\"{min_words[-1]}\" was the least valuable word with a value of {min(values)}.")
 print(f"The average word value is {round(numpy.mean(values))}.")
