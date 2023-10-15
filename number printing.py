708895d9aba27db22c706e2580397dca5fdb75b3
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-10-15 20:59:37 +0300

    Update number printing.py
    The code now works in more scenarios.

--- number printing.py
+++ number printing.py
@@ -4,15 +4,31 @@ print("Which numbers do you want to print?")
 while True:
     try:
         print("Give me the starting integer number: ")
-        st = int(input())
+        st = input()
+        while not st.isnumeric():
+            print("Give me an integer!")
+            st = input()
+        st = int(st)
         print("Give me the ending integer number: ")
-        en = int(input())
-        if st > en:
-            print("The starting number has to be smaller than the ending number.")
+        en = input()
+        while not en.isnumeric():
+            print("Give me an integer!")
+            en = input()
+        en = int(en)
+        while st > en:
+            print("The ending number cannot be smaller than the starting number.")
+            print(f"Give me an integer smaller than of equal to {st}: ")
+            en = input()
+            while not en.isnumeric():
+                print("Give me an integer!")
+                en = input()
+            en = int(en)
+        en = int(en)
+        print("Here is the result:")
         while st <= en:
             print(st, end=" ")
             st += 1
-        conf = input("\nPress anything to continue or 'N' quit. ")
+        conf = input("\nPress anything to continue or 'N' to quit. ")
         if conf in brk:
             break
     except ValueError:
