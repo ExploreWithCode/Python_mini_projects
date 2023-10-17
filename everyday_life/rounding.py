163dfa885be7f478819fe18c2e85ddf581990686
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-10-17 21:47:32 +0300

    Update rounding.py

--- everyday_life/rounding.py
+++ everyday_life/rounding.py
@@ -1,7 +1,6 @@
 # Customized rounding of a number
 brk = ["N", "n"]
-asdf = ""
-while asdf is not brk:
+while True:
     num = float(input("Give me a number to round: "))
     r_n = int(input("By how many decimals do you want to round up the number you gave me? "))
     rounded = round(num, r_n)
