1bf9ffe261e117b2f336745bde98d2b1b7e8fd11
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-11-09 18:58:47 +0200

    Update Alphabet indexing2.py

--- everyday_life/indexing/Alphabet indexing2.py
+++ everyday_life/indexing/Alphabet indexing2.py
@@ -1,6 +1,15 @@
-inp = input("Enter a letter (uppercase or lowercase) and receive its order in the alphabet:\n")
+inp = input("Enter one letter (without spaces) and receive its order in the alphabet:\n")
 inp = inp.lower()
 alphabet = list("abcdefghijklmnopqrstuvwxyz")
+t = True
+while t:
+    if not inp.isalpha() or inp not in alphabet:
+        inp = input('Type a letter from the English alphabet:\n')
+    elif len(inp) != 1:
+        inp = input('Type no more or less than 1 character:\n')
+    else:
+        t = False
+
 for index, ch in enumerate(alphabet):
     if inp == ch:
         print(ch, '=', index+1)
