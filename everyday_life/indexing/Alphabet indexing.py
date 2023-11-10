fb46d7917160f3c970c007324f21307298942b6d
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2023-11-10 17:26:04 +0200

    Update Alphabet indexing.py
    added functionality and made it more responsive

--- everyday_life/indexing/Alphabet indexing.py
+++ everyday_life/indexing/Alphabet indexing.py
@@ -1,19 +1,23 @@
 inp = input("Enter a letter (uppercase or lowercase) and receive its order in the alphabet:\n")
+inp = inp.lower()
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 
 def func(txt):
-    val = 0
-    if not txt.isalpha():
-        print("Give me a letter!")
-    elif len(txt) != 1:
-        print("Give only ONE letter!")
-    txt = txt.lower()
+    t = True
+    while t:
+        if not txt.isalpha() or txt not in alphabet:
+            txt = input('Type a letter from the English alphabet:\n')
+        elif len(txt) != 1:
+            txt = input('Type no more or less than 1 character:\n')
+        else:
+            t = False
     for char in alphabet:
         if txt == char:
             val = alphabet.index(char) + 1
-    return val
+            print(f"\"{txt}\" has the value of {val}.")
+            break
+    return ''
 
 
-if func(inp) > 0:
-    print(f"\"{inp}\" has the value of {func(inp)}.")
+func(inp)
