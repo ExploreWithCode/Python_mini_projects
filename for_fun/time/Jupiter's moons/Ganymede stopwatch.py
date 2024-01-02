7d986b4e709fdb56cd1b5d12280c7a1f06a72cce
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2024-01-03 00:19:38 +0200

    Update Ganymede stopwatch.py

--- for_fun/time/Jupiter's moons/Ganymede stopwatch.py
+++ for_fun/time/Jupiter's moons/Ganymede stopwatch.py
@@ -2,7 +2,7 @@ import time
 from IPython.display import clear_output
 print("The solar time model is used for this stopwatch. \n More info here: https://lco.global/spacebook/sky/sidereal-time/"
       " | https://science.nasa.gov/jupiter/moons/ganymede/facts/ | \n https://en.wikipedia.org/wiki/Solar_time | https://en.wikipedia.org/wiki/Synodic_day")
-ratio = 7.1546
+ratio = 7.15455296
 print(f"A day on Ganymede, Jupiter's moon, lasts {ratio} Earth days.")
 ganday = 0
 while ganday <= 365:
