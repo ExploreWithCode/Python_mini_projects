268df793648d63895af423401cea8d638bfaed22
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2024-01-04 01:55:34 +0200

    Update Moon stopwatch.py

--- for_fun/time/Moon stopwatch.py
+++ for_fun/time/Moon stopwatch.py
@@ -4,18 +4,18 @@ print("The solar time model is used for this stopwatch. \n More info here: https
       "| https://en.wikipedia.org/wiki/Lunar_day | \n https://en.wikipedia.org/wiki/Solar_time | https://en.wikipedia.org/wiki/Synodic_day")
 ratio = ((29*24*60*60)+(12*60*60)+(44*60)+2.9) / (24*60*60)
 print(f'A day on Moon lasts {ratio} Earth days.')
-day = 0
-while day <= 365:
+moonday = 0
+while moonday <= 365:
     moonhour = 0
     while moonhour < 24:
         moonmins = 0
         while moonmins < 60:
             moonsec = 0
             while moonsec < 60:
-                print(f"Moon's stopwatch: {day} : {moonhour} : {moonmins} : {moonsec}", end='\x1b[2K')
+                print(f"Moon's stopwatch: {moonday} : {moonhour} : {moonmins} : {moonsec}", end='\x1b[2K')
                 time.sleep(ratio)
                 clear_output()
                 moonsec += 1
             moonmins += 1
         moonhour += 1
-    day += 1
+    moonday += 1
