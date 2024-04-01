c67c2bb83e5092cdfe5d164eb37ebaca3eaef654
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2024-04-01 12:19:25 +0300

    Update CoinGeckoAPI.py

--- Cryptocurrency/CoinGeckoAPI.py
+++ Cryptocurrency/CoinGeckoAPI.py
@@ -5,6 +5,8 @@ BTC = cg.get_coin_ohlc_by_id(id="bitcoin", vs_currency="USD", days="1")
 print(f"Bitcoin: {BTC[-1][4]} USD")
 ETH = cg.get_coin_ohlc_by_id(id="ethereum", vs_currency="USD", days="1")
 print(f"Ethereum: {ETH[-1][4]} USD")
+ETH_BTC = cg.get_coin_ohlc_by_id(id="ethereum", vs_currency="btc", days="1")
+print(f"ETH/BTC: {ETH_BTC[-1][4]}")
 
 print("\nVALUE\n____________________________________\r")
 val_btc = round(BTC[-1][4] * 1.23456789, 2)
