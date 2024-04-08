3359ebc9b51c38b529d46e2af30c93d540294b35
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2024-04-08 18:12:16 +0300

    Update get_coin_ohlc_by_id.py

--- Cryptocurrency/CoinGeckoAPI/pycoingecko/get_coin_ohlc_by_id.py
+++ Cryptocurrency/CoinGeckoAPI/pycoingecko/get_coin_ohlc_by_id.py
@@ -1,10 +1,11 @@
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
-inp = input("Give me the name of a cryptocurrency and get its price back:\n")
+inp_coin = input("Give me the name of a cryptocurrency:\n")
+inp_cur = input("Give me the ticker symbol of a currency to value it in:\n")
 # The 'ohlc' stands for 'open, high, low, close' and they correspond to coin[-1][1], coin[-1][2], coin[-1][3] and coin[-1][4] accordingly.
-coin = cg.get_coin_ohlc_by_id(id=inp, vs_currency="USD", days="1")
-print(f"Current price of {inp.capitalize()}: {coin[-1][4]} USD")
-print(f"Open: {coin[-1][1]} USD")
-print(f"High: {coin[-1][2]} USD")
-print(f"Low: {coin[-1][3]} USD")
-print(f"Close: {coin[-1][4]} USD")
+coin = cg.get_coin_ohlc_by_id(id=inp_coin.lower(), vs_currency=inp_cur, days="1")
+print(f"Current price of {inp_coin.capitalize()}: {coin[-1][-1]} {inp_cur.upper()}")
+print(f"Open: {coin[-1][1]} {inp_cur.upper()}")
+print(f"High: {coin[-1][2]} {inp_cur.upper()}")
+print(f"Low: {coin[-1][3]} {inp_cur.upper()}")
+print(f"Close: {coin[-1][4]} {inp_cur.upper()}")
