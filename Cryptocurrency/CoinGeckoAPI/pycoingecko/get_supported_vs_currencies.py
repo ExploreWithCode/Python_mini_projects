bce543761be98364fe8c25d95710c090bb251fc3
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2024-04-13 23:41:06 +0300

    Update get_supported_vs_currencies.py

--- Cryptocurrency/CoinGeckoAPI/pycoingecko/get_supported_vs_currencies.py
+++ Cryptocurrency/CoinGeckoAPI/pycoingecko/get_supported_vs_currencies.py
@@ -1,6 +1,6 @@
 from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
 # Get the list of currencies you can compare others against
-supp = cg.get_supported_vs_currencies()
+supp_vs_curr = cg.get_supported_vs_currencies()
 # Print the list
-print(supp)
+print(supp_vs_curr)
