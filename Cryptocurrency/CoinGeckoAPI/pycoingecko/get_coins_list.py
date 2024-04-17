2d4a4b8680f1d50626e7bfac3cab84e383b2739b
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2024-04-17 11:49:08 +0300

    Update get_coins_list.py

--- Cryptocurrency/CoinGeckoAPI/pycoingecko/get_coins_list.py
+++ Cryptocurrency/CoinGeckoAPI/pycoingecko/get_coins_list.py
@@ -3,5 +3,11 @@ cg = CoinGeckoAPI()
 # Get a list of cryptocurrencies in alphabetical order
 coins = cg.get_coins_list()
 # Organize the output
+print("Here is a list of coins and tokens in alphabetical order:\n")
 for i in coins:
-    print(i)
+    ic1 = str(i["symbol"]).upper()
+    '''ic0k = str(list(i)[0]).upper()'''
+    ic1k = str(list(i)[1]).upper()
+    ic2k = str(list(i)[2]).upper()
+    print(f"{ic1k}: {ic1} | {ic2k}: {i["name"]}")
+'''print(list(coins[0])[0])'''
