306a332ec797246efafc853d1efefae88dec5e1a
ExWiCo <109108536+ExploreWithCode@users.noreply.github.com>
2024-04-23 17:29:37 +0300

    Create get_coins_categories.py

---
+++ Cryptocurrency/CoinGeckoAPI/pycoingecko/get_coins_categories.py
@@ -0,0 +1,9 @@
+from pycoingecko import CoinGeckoAPI
+cg = CoinGeckoAPI()
+cats = cg.get_coins_categories()
+print("Here are some cryptocurrency categories:\n")
+num = 0
+for i in cats:
+    num = num+1
+    print(num)
+    print(i, "\n")
